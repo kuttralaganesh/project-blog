bb1e5ec5ab35db948a2f4f81d27e448b643f5fd9
kuttralaganesh <kuttrala@gmail.com>
2021-03-24 15:51:44 +0530

    Update blogpost-routing.module.ts

--- blog-project/src/app/blogpost/blogpost-routing.module.ts
+++ blog-project/src/app/blogpost/blogpost-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogpostDetailComponent } from './blogpost-detail/blogpost-detail.component';
+import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
 
 const routes: Routes = [
-  {path: 'blog', component: BlogpostListComponent},
-  {path: 'blog/:id', component: BlogpostDetailComponent}
+  // {path: 'blog', component: BlogpostListComponent},
+  // {path: 'blog/:id', component: BlogpostDetailComponent}
 ];
 
 @NgModule({
